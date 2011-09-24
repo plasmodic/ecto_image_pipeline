1ff1e443cdc7946f0155b2ddab35a191e454c09a
Kurt Konolige <konolige@willowgarage.com>
2011-09-24 11:02:15 -0700

    midstage on kinect pipeline

---
+++ cells/RectifierNC.cpp
@@ -0,0 +1,42 @@
+#include <ecto/ecto.hpp>
+#include <image_pipeline/pinhole_camera_model.h>
+
+using ecto::tendrils;
+
+namespace image_pipeline
+{
+  struct RectifierNC
+  {
+    static void
+    declare_params(tendrils& params)
+    {
+    }
+    static void
+    declare_io(const tendrils& params, tendrils& in, tendrils& out)
+    {
+      in.declare<PinholeCameraModel>("camera", "Camera model.").required(true);
+      in.declare<cv::Mat>("image", "The input image.").required(true);
+      out.declare<cv::Mat>("image", "The rectified image.");
+    }
+    void
+    configure(const tendrils& params, const tendrils& in, const tendrils& out)
+    {
+      camera = in["camera"]; // this is like a smart pointer to the output.
+      image_in = in["image"];
+      image_out = out["image"];
+      cv::Mat output;   // do this for non-copied output
+      *image_out = output;
+    }
+    int process(const tendrils& /*in*/, const tendrils& /*out*/)
+    {
+      camera->rectifyImage(*image_in,*image_out);
+      return ecto::OK;
+    }
+    ecto::spore<PinholeCameraModel> camera;
+    ecto::spore<cv::Mat> image_in, image_out;
+
+  };
+}
+
+ECTO_CELL(image_pipeline, image_pipeline::RectifierNC, "Rectifier",
+          "Given a PinholeCameraModel, rectify the input image; output reuses buffer");
---
+++ cells/scripts/kinect_register.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import ecto
+from ecto_opencv import highgui
+from ecto_opencv.highgui import V4LCapture, VideoCapture, imshow, FPSDrawer, NiConverter
+from image_pipeline import Rectifier, CalibrationLoader
+
+cal_rgb = CalibrationLoader(filename='cal_rgb.yml')
+cal_depth = CalibrationLoader(filename='cal_depth.yml')
+rect_rgb = Rectifier()
+rect_depth = Rectifier()
+
+verter = highgui.NiConverter('verter')
+fps = highgui.FPSDrawer('fps')
+
+def kinect_vga(device_n):
+    from ecto_openni import Capture, ResolutionMode, Device
+    return Capture('ni device', rgb_resolution=ResolutionMode.VGA_RES,
+                   depth_resolution=ResolutionMode.VGA_RES,
+                   rgb_fps=30, depth_fps=30,
+                   device_number=device_n,
+                   registration=False,
+                   synchronize=False,
+                   device=Device.KINECT
+                   )
+
+kcap = kinect_vga(0);
+
+plasm = ecto.Plasm()
+
+#pipleline
+plasm.connect(
+    kcap[:] >> verter[:],
+#    verter['image'] >> fps[:],
+    verter['image'] >> rect_rgb['image'],
+    cal_rgb['camera'] >> rect_rgb['camera'],      
+    verter['depth'] >> rect_depth['image'],
+    cal_depth['camera'] >> rect_depth['camera'],      
+    )
+
+#display stuff
+# plasm.connect(
+#     rect_rgb['image'] >> imshow(name='Rectified RGB')['image'],
+#     rect_depth['image'] >> imshow(name='Rectified Depth')['image'],
+#     verter['image'] >> imshow(name='Original')['image']
+#     )
+
+#plasm.insert(kcap)
+
+if __name__ == '__main__':
+    from ecto.opts import doit
+    doit(plasm, description='Capture Kinect depth and RGB and register them.')
