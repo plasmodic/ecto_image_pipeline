188e096a64a38c2e547ee88e08a06341a95e5347
Kurt Konolige <konolige@willowgarage.com>
2011-10-17 18:02:07 -0700

    added enums file

---
+++ include/image_pipeline/enums.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace image_pipeline
+{
+  enum InterpolationMode
+  {
+    CV_INTER_NN=CV_INTER_NN, CV_INTER_LINEAR=CV_INTER_LINEAR, CV_INTER_CUBIC=CV_INTER_CUBIC, 
+    CV_INTER_AREA=CV_INTER_AREA
+  };
+
+}
