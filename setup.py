84ad333868ee2c633f3c795b9232f0a229f7eb41
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 23:35:23 -0700

    let cakin handle the Python and make sure the tests pass

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Ecto image pipeline',
+      version='1.0.0',
+      description='Utilities for using the image pipilend with ecto',
+      packages=['ecto_image_pipeline'],
+      package_dir={'':'python'}
+)
