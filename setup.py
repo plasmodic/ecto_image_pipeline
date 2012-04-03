329cd728c3ffcd8738ca80778fa62e5ffe78675f
Vincent Rabaud <vrabaud@willowgarage.com>
2012-04-03 06:30:54 -0700

    add more Python folders

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(name='Ecto image pipeline',
       version='1.0.0',
       description='Utilities for using the image pipilend with ecto',
-      packages=['ecto_image_pipeline'],
+      packages=['ecto_image_pipeline', 'ecto_image_pipeline.io', 'ecto_image_pipeline.io.source'],
       package_dir={'':'python'}
 )
