b71ea6e2b6a60792bd2fd45e86005780c848f878
Vincent Rabaud <vrabaud@willowgarage.com>
2012-11-01 07:09:30 -0700

    get the information from the package.xml

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-setup(name='Ecto image pipeline',
-      version='1.0.0',
-      description='Utilities for using the image pipilend with ecto',
-      packages=['ecto_image_pipeline', 'ecto_image_pipeline.io', 'ecto_image_pipeline.io.source'],
-      package_dir={'':'python'}
-)
+from distutils.core import setup
+from ecto.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['ecto_image_pipeline', 'ecto_image_pipeline.io', 'ecto_image_pipeline.io.source']
+d['package_dir'] = {'': 'python'}
+d['install_requires'] = []
+
+setup(**d)
