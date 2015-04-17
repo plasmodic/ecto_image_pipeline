d2dea485e86bfd05a2d355d5a72b2922f13db087
nlyubova <nlyubova@aldebaran-robotics.com>
2015-04-17 11:04:15 +0200

    fixing the depth rescale and its calibration matrix

--- cells/RescaleRegisteredDepth.cpp
+++ cells/RescaleRegisteredDepth.cpp
@@ -14,13 +14,16 @@ struct RescaledRegisteredDepth {
     in.declare(&RescaledRegisteredDepth::depth_in_, "depth", "The depth image to rescale.").required(true);
     in.declare(&RescaledRegisteredDepth::mask_in_, "mask", "A mask of the size of depth, that will be resized too.");
     in.declare(&RescaledRegisteredDepth::image_in_, "image", "The rgb image.").required(true);
+    in.declare(&RescaledRegisteredDepth::K_in_, "K", "The camera intrinsics matrix of the depth camera to rescale.");
 
     out.declare(&RescaledRegisteredDepth::depth_out_, "depth", "The rescaled depth image.");
     out.declare(&RescaledRegisteredDepth::mask_out_, "mask", "The rescaled depth image.");
+    out.declare(&RescaledRegisteredDepth::K_out_, "K", "The rescaled camera intrinsics matrix of the depth camera.");
   }
 
   int process(const tendrils& in, const tendrils& out) {
     cv::Size dsize = depth_in_->size(), isize = image_in_->size();
+    K_in_->copyTo(*K_out_);
 
     if (dsize == isize) {
       rescaleDepth(*depth_in_, CV_32F, *depth_out_);
@@ -43,6 +46,12 @@ struct RescaledRegisteredDepth {
             std::numeric_limits<float>::quiet_NaN()));
     *depth_out_ = output;
 
+    //rescale the calibration matrix
+    K_out_->at<float>(0, 0) = K_out_->at<float>(0, 0) * factor;
+    K_out_->at<float>(1, 1) = K_out_->at<float>(1, 1) * static_cast<float>(isize.height) / static_cast<float>(dsize.height);
+    K_out_->at<float>(0, 2) = static_cast<float>(isize.width-1) / 2.0f;
+    K_out_->at<float>(1, 2) = static_cast<float>(isize.height-1) / 2.0f;
+
     if (!mask_in_->empty()) {
       assert(mask_in_->size() == depth_in_->size());
       cv::Mat mask(isize, CV_8U);
@@ -55,7 +64,7 @@ struct RescaledRegisteredDepth {
 
     return ecto::OK;
   }
-  ecto::spore<cv::Mat> image_in_, depth_in_, depth_out_, mask_in_, mask_out_;
+  ecto::spore<cv::Mat> image_in_, depth_in_, depth_out_, mask_in_, mask_out_, K_in_, K_out_;
 };
 
 ECTO_CELL(base, RescaledRegisteredDepth, "RescaledRegisteredDepth",
