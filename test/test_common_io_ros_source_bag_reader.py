dbbf222acf21b628c107a6968e2e52b969e63844
Vincent Rabaud <vincent.rabaud@gmail.com>
2015-04-27 20:58:38 +0200

    convert tests to proper nosetests

--- test/test_common_io_ros_source_bag_reader.py
+++ test/test_common_io_ros_source_bag_reader.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 from ecto_image_pipeline.io.source.ros import BagReader
 from ecto_image_pipeline.io.source.camera_base import _assert_source_interface
-br = BagReader()
-print br.__doc__
-assert 'image_message' in br.__doc__
-assert 'depth' in br.__doc__
-assert 'image' in br.__doc__
-assert 'K' in br.__doc__
+import unittest
 
-#verify some types.
-assert 'boost::shared_ptr<sensor_msgs::Image_<std::allocator<void> > const>' == br.outputs.at('image_message').type_name
+class TestSourceBagReader(unittest.TestCase):
 
-#this should pass our interface check
-_assert_source_interface(br)
+    def test_interface(self):
+        br = BagReader()
 
+        print br.__doc__
+        assert 'image_message' in br.__doc__
+        assert 'depth' in br.__doc__
+        assert 'image' in br.__doc__
+        assert 'K' in br.__doc__
 
-#test the bag file name parameter
-br = BagReader(bag='testy.bag')
-assert br.params.bag == 'testy.bag'
-assert br.source.params.bag == 'testy.bag'
-_assert_source_interface(br)
+        #verify some types.
+        assert 'boost::shared_ptr<sensor_msgs::Image_<std::allocator<void> > const>' == br.outputs.at('image_message').type_name
+
+        #this should pass our interface check
+        _assert_source_interface(br)
+
+        #test the bag file name parameter
+        br = BagReader(bag='testy.bag')
+        assert br.params.bag == 'testy.bag'
+        assert br.source.params.bag == 'testy.bag'
+        _assert_source_interface(br)
+
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun('ecto_image_pipeline', 'test_source_bag_reader', TestSourceBagReader)
--- test/test_common_io_ros_source_kinect.py
+++ test/test_common_io_ros_source_kinect.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 from ecto_image_pipeline.io.source.ros import OpenNISubscriber
 from ecto_image_pipeline.io.source.camera_base import _assert_source_interface
-kr = OpenNISubscriber()
+import unittest
 
-assert 'image_message' in kr.__doc__
-assert 'depth' in kr.__doc__
-assert 'image' in kr.__doc__
-assert 'K' in kr.__doc__
+class TestSourceKinect(unittest.TestCase):
 
-#verify some types.
-assert 'boost::shared_ptr<sensor_msgs::Image_<std::allocator<void> > const>' == kr.outputs.at('image_message').type_name
+    def test_interfaces(self):
+        kr = OpenNISubscriber()
 
-#this should pass our interface check
-_assert_source_interface(kr)
+        assert 'image_message' in kr.__doc__
+        assert 'depth' in kr.__doc__
+        assert 'image' in kr.__doc__
+        assert 'K' in kr.__doc__
 
-# Make sure we can reassign topics
-new_topic = '/overriden_depth_image_topic'
-kr = OpenNISubscriber(depth_image_topic=new_topic)
-assert kr.params.depth_image_topic == new_topic
+        #verify some types.
+        assert 'boost::shared_ptr<sensor_msgs::Image_<std::allocator<void> > const>' == kr.outputs.at('image_message').type_name
+
+        #this should pass our interface check
+        _assert_source_interface(kr)
+
+        # Make sure we can reassign topics
+        new_topic = '/overriden_depth_image_topic'
+        kr = OpenNISubscriber(depth_image_topic=new_topic)
+        assert kr.params.depth_image_topic == new_topic
+
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun('ecto_image_pipeline', 'test_source_kinect', TestSourceKinect)
